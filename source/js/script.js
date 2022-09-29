50730d1936f08ba8689f410d8b48c062fca6ba7d
Ekaterina <ektregubova@gmail.com>
2022-09-29 23:14:27 +0500

    style

---
+++ source/js/script.js
@@ -0,0 +1,18 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+//let mapImage = document.querySelector('.map__image');
+//let mapIframe = document.querySelector('.map__iframe');
+
+//mapImage.classList.add('map__image--nojs');
+//mapIframe.classList.add('map__iframe--js');
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
