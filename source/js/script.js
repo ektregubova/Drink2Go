19e0af77f015c307c926014cabf44bae5f2c039d
Ekaterina <ektregubova@gmail.com>
2022-10-03 23:39:24 +0500

    api

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,10 @@
 let mainNav = document.querySelector('.main-nav');
 let toggleNav = document.querySelector('.main-nav__toggle');
 let mapImage = document.querySelector('.map__image');
-let mapIframe = document.querySelector('.map__iframe');
+let mapLeaflet = document.querySelector('.map__leaflet');
 
 mapImage.classList.add('map__image--nojs');
-mapIframe.classList.add('map__iframe--js');
+mapLeaflet.classList.add('map--js');
 
 mainNav.classList.remove('main-nav--nojs');
 
@@ -17,3 +17,51 @@ toggleNav.addEventListener('click', function () {
     mainNav.classList.remove('main-nav--opened');
   }
 });
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    bulletElement: 'button',
+    bulletActiveClass:'swiper-pagination-bullet--active'
+  },
+}
+);
+
+const map = L.map("map")
+  .setView({
+    lat: 59.96838,
+    lng: 30.31740,
+  }, 18);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map)
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/icons/map-pin.svg',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+});
+
+const marker = L.marker(
+  {
+    lat: 59.96832,
+    lng: 30.31748,
+  },
+  {
+    icon: mainPinIcon,
+  },
+);
+
+marker.addTo(map);
